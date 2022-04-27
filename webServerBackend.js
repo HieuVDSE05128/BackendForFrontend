e5105e78a208db98ba830af8ef79dd578293ab01
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2022-04-27 10:29:26 +0700

    update web server backend

--- webServerBackend.js
+++ webServerBackend.js
@@ -7,14 +7,7 @@ const http = require('http');
 const WEB_PORT = 3001;
 const server = http.createServer(app);
 
-app.use('/getAll', (req, res, next) => {
-    assRepository.getListAsset(req, res, next);
-})
-
-app.use('/getById', (req, res, next) => {
-    const id = Number(req.query.id)
-    assRepository.getAssetById(req, res, next);
-})
+app.use(webRouter);
 
 server.listen(WEB_PORT, () => {
     console.log(`web port list on port ${WEB_PORT}`)
